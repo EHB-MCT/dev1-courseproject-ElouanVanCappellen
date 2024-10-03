900166920589651c5a48c3106939afff8e3acb01
Elouan Van Cappellen <elouan.van.cappellen@student.ehb.be>
2024-10-03 14:51:19 +0200

    oefening verbeetert

--- spaceInvader.js
+++ spaceInvader.js
@@ -2,27 +2,23 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext("2d");
-"use strict";
 
-backGround();
+background();
 signature();
 
 function backGround() {
     context.fillStyle = "black"
-    context.beginPath();
-    context.rect(50, 50, 350, 350)
-    context.fill();
+    context.fillRectrect(50, 50, 350, 350)
 }
+
 function signature() {
     context.fillStyle = "#d8d97e"
-    context.beginPath();
-    context.rect(100, 150, 50, 100);
-    context.rect(300, 150, 50, 100);
-    context.rect(150, 200, 50, 150);
-    context.rect(250, 200, 50, 150);
-    context.rect(200, 150, 50, 50);
-    context.rect(200, 250, 50, 50);
-    context.rect(100, 300, 50, 50);
-    context.rect(300, 300, 50, 50);
-    context.fill();
+    context.fillRect(100, 150, 50, 100);
+    context.fillRect(300, 150, 50, 100);
+    context.fillRect(150, 200, 50, 150);
+    context.fillRect(250, 200, 50, 150);
+    context.fillRect(200, 150, 50, 50);
+    context.fillRect(200, 250, 50, 50);
+    context.fillRect(100, 300, 50, 50);
+    context.fillRect(300, 300, 50, 50);
 }
